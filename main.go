c91f5698249daf1bdac7b1a5f07acaa61eeb6d30
Dakota Feist <dakotafeist@protonmail.com>
2026-06-14 16:17:49 -0500

    Updated to connect directly to iwd

--- main.go
+++ main.go
@@ -1,56 +1,38 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"log"
-	"time"
 
-	//"net"
-
-	"github.com/mdlayher/wifi"
+	"github.com/godbus/dbus/v5"
 )
 
 func main() {
-	//gets all interfaces
-	//Creates a new client for scanning
-	client, err := wifi.New()
+	conn, err := dbus.ConnectSystemBus()
 	if err != nil {
-		return
+		panic(err)
 	}
-	defer client.Close()
-	// Get all available Wi-Fi interfaces
-	interfaces, err := client.Interfaces()
-	if err != nil {
-		log.Fatal(err)
-	}
-	// Use the first available interface for scanning
-	if len(interfaces) == 0 {
-		log.Fatal("No Wi-Fi interfaces found")
-	}
-	ifi := interfaces[1]
-	fmt.Printf("Using interface: %s (%s)\n", ifi.Name, ifi.HardwareAddr)
+	defer conn.Close()
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
+	obj := conn.Object(
+		"net.connman.iwd",
+		"/",
+	)
 
-	// Request a scan on the selected interface
-	err = client.Scan(ctx, ifi)
+	var managedObjects map[dbus.ObjectPath]map[string]map[string]dbus.Variant
+
+	err = obj.Call(
+		"org.freedesktop.DBus.ObjectManager.GetManagedObjects",
+		0,
+	).Store(&managedObjects)
+
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
-	// Might need some async magic to let the client.Scan call finish
-	aps, err := client.AccessPoints(ifi)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Available Access Points:")
-	fmt.Printf("%-32s %-18s %-5s %-18s\n", "SSID", "BSSID", "Signal", "AKM")
-	for _, ap := range aps {
-		if ap.SSID != "" {
-			fmt.Printf("%-32s %-18s %d%% %v\n", ap.SSID, ap.BSSID.String(), 2*((ap.Signal/100)+100), ap.RSN.AKMs)
+	for path, interfaces := range managedObjects {
+		fmt.Println(path)
+		for iface := range interfaces {
+			fmt.Println("  ", iface)
 		}
 	}
 }
